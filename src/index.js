87aa442330c965172d007acad326f6578a785824
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2018-04-23 19:41:04 +0300

    1.5 yksi olio

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const  App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat =[ {
-        nimi: "Reactin perusteet",
-        tehtavia: 10
-    },
-    {
-        nimi:  "Tiedonvälitys propseilla",
-        tehtavia: 7
-    },
-     {
-        nimi: "Komponenttien tila",
-        tehtavia: 14
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: "Reactin perusteet",
+                tehtavia: 10
+            },
+            {
+                nimi: "Tiedonvälitys propseilla",
+                tehtavia: 7
+            },
+            {
+                nimi: "Komponenttien tila",
+                tehtavia: 14
+            }
+        ]
     }
-]
-    
+
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osat = { osat} />
-            <Yhteensa yhteensa={ osat } />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa yhteensa={kurssi.osat} />
         </div>
 
     )
@@ -29,29 +32,29 @@ const  App = () => {
 const Otsikko = (props) => {
     return (
         <div>
-            <h1>{props.kurssi}</h1>
+            <h1>{props.kurssi.nimi}</h1>
         </div>
     )
 
 }
 
-const  Osa = (props) => {
+const Osa = (props) => {
     return (
         <div>
             <p>{props.osa} {props.tehtava}</p>
-            
-         </div>
+
+        </div>
 
     )
 
 }
-const  Sisalto = (props) => {
+const Sisalto = (props) => {
     return (
         <div>
-          <Osa osa={props.osat[0].nimi} tehtava={props.osat[0].tehtavia}/>
-          <Osa osa={props.osat[1].nimi} tehtava={props.osat[1].tehtavia}/>
-          <Osa osa={props.osat[2].nimi} tehtava={props.osat[2].tehtavia}/>
-            
+            <Osa osa={props.osat[0].nimi} tehtava={props.osat[0].tehtavia} />
+            <Osa osa={props.osat[1].nimi} tehtava={props.osat[1].tehtavia} />
+            <Osa osa={props.osat[2].nimi} tehtava={props.osat[2].tehtavia} />
+
         </div>
 
     )
@@ -60,9 +63,9 @@ const  Sisalto = (props) => {
 
 const Yhteensa = (props) => {
     return (
-    <div>
-        <p>Yhteensä: {props.yhteensa[0].tehtavia + props.yhteensa[1].tehtavia + props.yhteensa[2].tehtavia}</p>
-    </div>
+        <div>
+            <p>Yhteensä: {props.yhteensa[0].tehtavia + props.yhteensa[1].tehtavia + props.yhteensa[2].tehtavia}</p>
+        </div>
     )
 }
 
